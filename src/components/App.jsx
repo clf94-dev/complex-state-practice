41bfd1ec0328410c5a51f6e06b8bfa29967fe3ae
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-05 10:28:28 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    //Tienen que ser asi ^
+    console.log(name);
+    console.log(value);
+    setContact(prevValue => {
+      switch (name) {
+        case "fName":
+          return {
+            fName: value,
+            lName: prevValue.lName,
+            email: prevValue.email
+          };
+
+        case "lName":
+          return {
+            fName: prevValue.fName,
+            lName: value,
+            email: prevValue.email
+          };
+
+        case "email":
+          return {
+            fName: prevValue.fName,
+            lName: prevValue.lName,
+            email: value
+          };
+
+        default:
+          break;
+      }
+      console.log(contact);
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
